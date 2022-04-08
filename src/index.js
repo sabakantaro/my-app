7a9aeba78f2b0411e567b2057d3b4d6cf85c2cfd
sabakantaro <sbkryo290112@yahoo.co.jp>
2022-04-09 00:42:14 +0900

    ボードゲーム機能の実装

--- src/index.js
+++ src/index.js
@@ -3,27 +3,42 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  render() {
-    return (
-      <button className="square" onClick={() => this.props.onClick()}>
-        {this.props.value}
-      </button>
-    );
-  }
+// class Square extends React.PureComponent {
+//   render() {
+//     return (
+//       <button className="square" onClick={() => this.props.onClick()}>
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+// クラスから関数コンポーネントへ書き換え
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
 }
 
-class Board extends React.Component {
+class Board extends React.PureComponent {
   constructor() {
     super();
     this.state = {
-      squares: Array(9).fill('愛'),
+      squares: Array(9).fill(null),
+      whoIsNext: true,
     };
   }
   handleClick(i) {
     const squares = this.state.squares.slice(); //sliceでsquaresの配列を分割して渡す
-    squares[i] = 'H';
-    this.setState({ squares: squares });
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    squares[i] = this.state.whoIsNext ? 'H' : '愛';
+    this.setState({
+      squares: squares,
+      whoIsNext: !this.state.whoIsNext,
+    });
   }
   renderSquare(i) {
     return (
@@ -35,10 +50,17 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'あなたの脳内';
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = '強き煩悩：' + winner;
+    } else {
+      status = 'あなたの脳内：' + (this.state.whoIsNext ? 'H' : '愛');
+    }
 
     return (
       <div>
+        <div className="bonnou">最強の煩悩決定戦</div>
         <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
@@ -60,7 +82,7 @@ class Board extends React.Component {
   }
 }
 
-class Game extends React.Component {
+class Game extends React.PureComponent {
   render() {
     return (
       <div className="game">
@@ -91,3 +113,23 @@ ReactDOM.render(<Game />, document.getElementById('root'));
 // const container = document.getElementById('root');
 // const root = ReactDOM.createRoot(container);
 // root.render(<AppWithCallbackAfterRender />);
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
